060f0343f4307160a8701e6ded24b988b511d0a1
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-01-07 13:16:12 +0600

    ENV Issues Fixed √

--- tests/tab/general.spec.js
+++ tests/tab/general.spec.js
@@ -1,12 +1,13 @@
 const { test, expect } = require('@playwright/test');
 
-test.use({ storageState: 'playwright/.auth/user.json' });
+test.describe("SchedulePress General Tab All TestCases", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://schedulepress.qa1.site/wp-admin/admin.php?page=schedulepress');
+  });
 
-test('Verify Login', async ({ page }) => {
-  const cookies = await page.context().cookies();
-
-  await page.goto('https://schedulepress.qa1.site/wp-admin/admin.php?page=schedulepress');
-  await expect(page.getByRole('img', { name: 'mainLogo' })).toBeVisible();
-  await expect(page.getByText('Core Version:')).toBeVisible();
-  await expect(page.getByText('Pro Version:')).toBeVisible();
+  test('Verify Login', async ({ page }) => {
+    await expect(page.getByRole('img', { name: 'mainLogo' })).toBeVisible();
+    await expect(page.getByText('Core Version:')).toBeVisible();
+    await expect(page.getByText('Pro Version:')).toBeVisible();
+  });
 });
