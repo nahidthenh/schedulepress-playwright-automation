5e37a62bce4f048ca8c916d2ef463105e6e991ee
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-01-07 16:26:10 +0600

    USerJSOn Update

--- tests/tab/general.spec.js
+++ tests/tab/general.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test.use({ storageState: 'playwright/.auth/user.json' });
 
-test.describe("SchedulePress General Tab All TestCases", () => {
+test.describe("SchedulePress General Tab All TestCases ", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://schedulepress.qa1.site/wp-admin/admin.php?page=schedulepress');
     await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
