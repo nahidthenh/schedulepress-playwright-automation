850c5d80c48772a4422e2a1f8d8c2072d161faaf
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-01-07 13:45:29 +0600

    Setup Slack Report and Workflows

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,39 @@
 const { defineConfig, devices } = require('@playwright/test');
+require('dotenv').config();
+const isCI = !!process.env.CI; // Check if the CI environment variable is set
 
 module.exports = defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  forbidOnly: isCI,
+  retries: isCI ? 2 : 1,
+  workers: isCI ? 1 : undefined,
+  reporter: [
+    ...(isCI
+      ? [
+        [
+          './node_modules/playwright-slack-report/dist/src/SlackReporter.js',
+          {
+            slackWebHookUrl: process.env.SLACK_WEBHOOK_URL,
+            sendResults: 'always',
+            maxNumberOfFailuresToShow: 0,
+            meta: [
+              {
+                key: ":wpsp: SchedulePress Automation - Test Results",
+                value:
+                  "<https://nahidthenh.github.io/schedulepress-playwright-automation/ | :desktop_computer: Check The Final Report!>",
+              },
+            ],
+          },
+        ],
+      ]
+      : []),
+    ['html', { outputFolder: 'playwright-report', open: 'never' }], // Ensure you have the HTML reporter as well
+    ['dot'], // Console output reporter
+  ],
+  outputDir: 'test-results',
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    // baseURL: '',
     trace: 'on-first-retry',
   },
 
